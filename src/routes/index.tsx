019aaa59fe7606137d042fa92fc34559b319d314
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-19 18:03:54 -0300

    Adiciona a pagina de repositorio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
+import Repositories from '../pages/Repositories';
 
 export const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/gitcollection" component={Dashboard} />
+        <Route path="/repositories/:repository+" component={Repositories} />
       </Switch>
     </BrowserRouter>
   );
